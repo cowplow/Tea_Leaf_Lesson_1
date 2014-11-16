ba992b9364a56b1722b5f703068eff03ba7fe6a6
Chris Trembley <cmt2057@rit.edu>
2014-11-15 22:39:17 -0500

    Updated calculator.rb for a smarter input selection

--- calculator.rb
+++ calculator.rb
@@ -7,12 +7,24 @@ end
 begin
   say "Please enter a number:"
   num_1 = gets.chomp
-end while num_1.to_s != num_1.to_i.to_s
+
+  if num_1.to_i == 0  && num_1 != '0'
+    bad = true 
+  else
+    bad = false
+  end
+end while bad
 
 begin
   say "Please enter a 2nd number:"
   num_2 = gets.chomp
-end while num_2.to_s != num_2.to_i.to_s
+
+  if num_2.to_i == 0  && num_2 != '0'
+    bad = true 
+  else
+    bad = false
+  end
+end while bad
 
 begin
   say "Please enter the number of your desired function"
@@ -26,13 +38,13 @@ end while function.to_i < 1  || function.to_i > 4
 case function
   when '1'
     function = "Addition"
-    result = num_1.to_i + num_2.to_i
+    result = num_1.to_f + num_2.to_f
   when '2'
     function = "Subtraction"
-    result = num_1.to_i - num_2.to_i
+    result = num_1.to_f - num_2.to_f
   when '3'
     function = "Multiplication"
-    result = num_1.to_i * num_2.to_i
+    result = num_1.to_f * num_2.to_f
   else
     function = "Division"
     result = num_1.to_f / num_2.to_f
@@ -44,4 +56,5 @@ say "The result is #{result}"
 
 
 
+    
     
