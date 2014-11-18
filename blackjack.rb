c967d4bfc0f65248ce8a94bfb2e53631a9f21101
Chris Trembley <cmt2057@rit.edu>
2014-11-17 21:26:55 -0500

    A little bit better

--- blackjack.rb
+++ blackjack.rb
@@ -55,10 +55,19 @@ def hand_value(hand)
   end
 
   value
-end 
+end
+
+def hole_card(dealer_cards)
+  if hand_value(dealer_cards) == 21
+    true
+  else
+    false
+  end
+end
 
 begin
   busted = false
+  natural_21 = false
 
   player_cards = []
   dealer_cards = []
@@ -74,6 +83,20 @@ begin
 
   dealer_showing(dealer_cards)
 
+  case dealer_cards.first[0]
+  when "A"
+    natural_21 = hole_card(dealer_cards)
+  when "T"
+    natural_21 = hole_card(dealer_cards)
+  when "J"
+    natural_21 = hole_card(dealer_cards)
+  when "Q"
+    natural_21 = hole_card(dealer_cards)
+  when "K"
+    natural_21 = hole_card(dealer_cards)
+  end
+    
+
   begin
     player_choice = 'S'
     player_cards << deal_card(deck)
@@ -85,7 +108,17 @@ begin
       say "You bust, the Dealer wins this round."
       busted = true
       break
-    elsif (hand_value(player_cards) == 21)
+    elsif (hand_value(player_cards) == 21  && natural_21)
+      say "Both players have 21, it's a push!"
+      busted = true
+      break
+    elsif natural_21
+        say "You lose the Dealer has 21, what a luck sack!" 
+    elsif (hand_value(player_cards) == 21 && player_cards.length == 2)
+      say "Congratulations, you got a Black Jack"
+      natural_21 = true
+      break
+    elsif hand_value(player_cards) == 21
       say "Congratulations, you have 21!"
       break     
     else
@@ -101,21 +134,35 @@ begin
     end
   end until player_choice == 'S'
 
-  sleep 0.5
+  if !natural_21
+    sleep 0.5
 
-  say "The Dealer reveals the #{VALUES[dealer_cards[1][0]][1]} of #{SUITS[dealer_cards[1][1]]}"
+    say "The Dealer reveals the #{VALUES[dealer_cards[1][0]][1]} of #{SUITS[dealer_cards[1][1]]}"
   
-  sleep 0.5
+    sleep 0.5
 
-  say "The Dealer has a value of #{hand_value(dealer_cards)}"
+   say "The Dealer has a value of #{hand_value(dealer_cards)}"
+ end
 
-  if !busted
+  if !busted && !natural_21
     sleep 1
     comp_value = hand_value(dealer_cards)
+
+    while comp_value < 17
+      say "The Dealer's hand is less than 17, they take another card..."
+      dealer_cards << deal_card(deck)
+      sleep 1
+      say "The Dealer reveals the #{VALUES[dealer_cards[-1][0]][1]} of #{SUITS[dealer_cards[-1][1]]}"
+      comp_value = hand_value(dealer_cards)
+      say "The Dealer now has a value of #{comp_value}"
+      sleep 1
+    end
+
+
     if hand_value(player_cards) > comp_value && !bust?(comp_value)
       say "Player has the higher hand this round!"
     elsif bust?(comp_value)
-      say "Player wins, Dealer busts!"
+      say "Player wins, the Dealer busts!"
     elsif hand_value(player_cards) == comp_value
       say "This round is a push"
     else
