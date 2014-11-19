ebf80bacda8c0429c45a7151767bcf2c11c6af57
Chris Trembley <cmt2057@rit.edu>
2014-11-18 20:08:33 -0500

    Added all the bonus tasks, additionally added the ability to double down

--- blackjack.rb
+++ blackjack.rb
@@ -5,12 +5,14 @@ VALUES = { "A" => [11,"Ace"], "2" => [2, "Two"], "3" => [3, "Three"], "4" => [4,
             "9" => [9, "Nine"], "T" => [10, "Ten"], "J" => [10, "Jack"], "Q" => [10, "Queen"],
             "K" => [10, "King"]}
 
-def shuffle_deck
+def shuffle_deck(deck_count)
   new_deck = []
   VALUES.each do |key, value|
     SUITS.each do |letter, word|
-      card = "#{key}#{letter}"
-      new_deck << card
+      deck_count.to_i.times do
+        card = "#{key}#{letter}"
+        new_deck << card
+      end
     end
   end
   new_deck.shuffle!
@@ -58,21 +60,35 @@ def hand_value(hand)
 end
 
 def hole_card(dealer_cards)
-  if hand_value(dealer_cards) == 21
-    true
-  else
-    false
-  end
+  hand_value(dealer_cards) == 21
 end
+
+def dbl_down(player_cards, deck)
+  say "You take 1 final card"
+  player_cards << deal_card(deck)
+  say "You were dealt the #{VALUES[player_cards.last[0]][1]} of #{SUITS[player_cards.last[1]]}"
+  return hand_value(player_cards)
+end
+
+say "Welcome to TeaLeaf Black Jack.  Can we have your name please?"
+player_name = gets.chomp
+
+begin
+  say "How many decks would you like to play with? (1-999)"
+  deck_count = gets.chomp
+end until deck_count.to_i < 1000
+
+deck = shuffle_deck(deck_count)
 
 begin
   busted = false
   natural_21 = false
+  double_down = false
 
   player_cards = []
   dealer_cards = []
 
-  deck = shuffle_deck
+  deck = shuffle_deck(deck_count) if deck.length < 30
 
   player_cards << deal_card(deck)
   say "You were dealt the #{VALUES[player_cards.last[0]][1]} of #{SUITS[player_cards.last[1]]}"
@@ -102,7 +118,10 @@ begin
     player_cards << deal_card(deck)
     say "You were dealt the #{VALUES[player_cards.last[0]][1]} of #{SUITS[player_cards.last[1]]}"
     sleep 1
-    say "Your hand now has a value of #{hand_value(player_cards)}. The Dealer is showing a value of #{VALUES[dealer_cards[0][0]][0]}"
+    puts "\n
+    *-------------------------------------------------------------*
+    |Your Hand:#{hand_value(player_cards)}                                 Dealer's Hand:#{VALUES[dealer_cards[0][0]][0]} |
+    *-------------------------------------------------------------*"
     sleep 1
     if bust?(hand_value(player_cards))
       say "You bust, the Dealer wins this round."
@@ -114,6 +133,7 @@ begin
       busted = true
       break
     elsif natural_21
+        say "The Dealer reveals their hole card, the #{VALUES[dealer_cards[1][0]][1]} of #{SUITS[dealer_cards[1][1]]}"
         say "You lose the Dealer has 21, what a luck sack!" 
     elsif (hand_value(player_cards) == 21 && player_cards.length == 2)
       say "Congratulations, you have a Black Jack"
@@ -124,29 +144,42 @@ begin
       say "Congratulations, you have 21!"
       break     
     else
-      say "Would you like to Hit or Stay? H for hit, S for Stay"
-      player_choice = gets.chomp.upcase
+      if player_cards.length == 2
+        say "Would you like to Hit, Stay or Double Down. (H/S/D)"
+        player_choice = gets.chomp.upcase
+        if player_choice != 'H' && player_choice !='S' && player_choice != 'D'
+          begin
+            say "Invalid command. H for Hit, S for Stay, D for Double Down"
+            player_choice = gets.chomp.upcase
+          end until player_choice == 'H' || player_choice =='S' || player_choice == 'D'
+        elsif player_choice == 'D'
+          if bust?(dbl_down(player_cards, deck))
+            say "You bust, the Dealer wins this round."
+            busted = true
+          end
+          double_down = true
+        end
+      else
+        say "Would you like to Hit or Stay? H for hit, S for Stay"
+        player_choice = gets.chomp.upcase
 
-      if player_choice != 'H' && player_choice !='S'
-        begin
-          say "Invalid command. H for Hit, S for Stay"
-          player_choice = gets.chomp.upcase
-        end until player_choice == 'H' || player_choice =='S'
-      end  
+        if player_choice != 'H' && player_choice !='S'
+          begin
+            say "Invalid command. H for Hit, S for Stay"
+            player_choice = gets.chomp.upcase
+          end until player_choice == 'H' || player_choice =='S'
+        end  
+      end
     end
-  end until player_choice == 'S'
+  end until player_choice == 'S' || player_choice == 'D'
 
-  if !natural_21
-    sleep 0.5
-
+  if ( !busted && !natural_21 ) || (double_down && !busted)
     say "The Dealer reveals the #{VALUES[dealer_cards[1][0]][1]} of #{SUITS[dealer_cards[1][1]]}"
-  
     sleep 0.5
-
-   say "The Dealer has a value of #{hand_value(dealer_cards)}"
- end
-
-  if !busted && !natural_21
+    puts "\n
+      *-------------------------------------------------------------*
+      |Your Hand:#{hand_value(player_cards)}                                 Dealer's Hand:#{hand_value(dealer_cards)} |
+      *-------------------------------------------------------------*"
     sleep 1
     comp_value = hand_value(dealer_cards)
 
@@ -156,22 +189,28 @@ begin
       sleep 1
       say "The Dealer reveals the #{VALUES[dealer_cards[-1][0]][1]} of #{SUITS[dealer_cards[-1][1]]}"
       comp_value = hand_value(dealer_cards)
-      say "The Dealer now has a value of #{comp_value}"
+      puts "\n
+        *-------------------------------------------------------------*
+        |Your Hand:#{hand_value(player_cards)}                                 Dealer's Hand:#{comp_value} |
+        *-------------------------------------------------------------*"
       sleep 1
     end
 
 
     if hand_value(player_cards) > comp_value && !bust?(comp_value)
-      say "Player has the higher hand this round!"
+      say "#{player_name} has the higher hand this round!"
     elsif bust?(comp_value)
-      say "Player wins, the Dealer busts!"
+      say "#{player_name} wins, the Dealer busts!"
     elsif hand_value(player_cards) == comp_value
       say "This round is a push"
     else
-      say "Sorry you lose this round, the Dealer has the higher hand."
+      say "Sorry #{player_name}, you lose this round, the Dealer has the higher hand."
     end
   end
-        
-
-end
+  sleep 2
+  begin        
+    say "Would you like to play another hand? (Y/N)"
+    again = gets.chomp.downcase
+  end until again == 'y' || again == 'n'
+end until again == 'n'
 
